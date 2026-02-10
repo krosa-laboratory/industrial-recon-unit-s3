d51a65baad79aee249c094297ea83c3f6a4a55d2
piratike <kevinmachuca96@hotmail.com>
2026-02-10 19:27:55 +0000

    Prepared a test for an ESP32-WROOM-32 NodeMCU to test. Maybe I develop the prototype in breadboard and let the PCB unused because it has problems with USB comms.

--- Software/Industrial Protocol Auditor Firmware/src/main.cpp
+++ Software/Industrial Protocol Auditor Firmware/src/main.cpp
@@ -1,18 +1,15 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+void setup()
+{
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    delay(1000);
+    Serial.println("Booting...");
+    delay(2000);
+    Serial.println("Hello World!");
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() {}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
